7e089709bf4eb710cf4d3aa3301e98e9cecf68f8
Seth Thomas <sethpthomas91@gmail.com>
2022-12-27 16:17:15 -0500

    add tailwind, nav, header, footer

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/ui/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    },
+    colors: {
+      description: '#54595F',
+      white: '#fff',
+      black: '#000',
+      grey: {
+        100: '#cfd8dc'
+      },
+      blue: {
+        400: '#5c6bc0'
+      },
+      red: {
+        400: '#ef5350',
+        500: '#f44336'
+      }
+    }
+  },
+  plugins: [],
+}
